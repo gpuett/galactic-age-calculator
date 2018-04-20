7dd9fcf49da79417c53e5cc2a712a75f3ad361ca
Garnett Puett <gpuett@gmail.com>
2018-04-20 11:50:25 -0700

    add years on each planet front end.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $("#seconds").click(function(event) {
+  $("#seconds").click(function() {
     let age = new Age($("#age").val());
-    $("#outputSeconds").text(`You are ${age.inSeconds()} seconds old`);
-
-  })
+    $("#outputSeconds").text(`You are ${age.inSeconds()} seconds old.`);
+  });
+  $("#mercury").click(function() {
+    let age = new Age($("#age").val());
+    $("#outputMercury").text(`You would be ${age.mercury()} years old on Mercury.`);
+  });
+  $("#venus").click(function() {
+    let age = new Age($("#age").val());
+    $("#outputVenus").text(`You would be ${age.venus()} years old on Venus.`);
+  });
+  $("#mars").click(function() {
+    let age = new Age($("#age").val());
+    $("#outputMars").text(`You would be ${age.mars()} years old on Mars.`);
+  });
+  $("#jupiter").click(function() {
+    let age = new Age($("#age").val());
+    $("#outputJupiter").text(`You would be ${age.jupiter()} years old on Jupiter.`);
+  });
 });
